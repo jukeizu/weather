cdeda542bad1f34277101935a930a2a917c2b3a2
Shawn Toffel <shawn.toffel@gmail.com>
2019-02-18 02:14:51 +0000

    cleanup

--- treediagram/handler.go
+++ treediagram/handler.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jukeizu/contacts/api/protobuf-spec/contactspb"
 	"github.com/jukeizu/contract"
 	"github.com/jukeizu/weather/api/protobuf-spec/weatherpb"
 	shellwords "github.com/mattn/go-shellwords"
@@ -135,26 +134,4 @@ func (h *handler) makeLoggingHttpHandlerFunc(name string, f func(contract.Reques
 
 		contractHandlerFunc.ServeHTTP(w, r)
 	}
-}
-
-func parseNameValue(command string, content string) (string, string) {
-	input := strings.SplitAfterN(content, command, 2)[1]
-	split := strings.SplitN(input, "'", 3)
-	name, value := split[1], strings.TrimSpace(split[2])
-
-	return name, value
-}
-
-func formatContact(contact *contactspb.Contact) string {
-	if contact == nil {
-		return ""
-	}
-
-	buffer := bytes.Buffer{}
-	buffer.WriteString(fmt.Sprintf("**%s**\n", contact.Name))
-	buffer.WriteString(fmt.Sprintf(":house: %s", contact.Address))
-	buffer.WriteString(fmt.Sprintf("\n\n:iphone: %s", contact.Phone))
-	buffer.WriteString("\n\n\n")
-
-	return buffer.String()
 }
